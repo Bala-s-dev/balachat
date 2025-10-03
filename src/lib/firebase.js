f3a95d63d5e90fb16ca03487bc9f8b6020afdfa5
Bala-s-dev <baladev2004@gmail.com>
2025-10-03 10:45:33 +0530

    final update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "webchat-1cee7.firebaseapp.com",
-  projectId: "webchat-1cee7",
-  storageBucket: "webchat-1cee7.appspot.com",
-  messagingSenderId: "21554291273",
-  appId: "1:21554291273:web:e5926e20cec0b1595b5598"
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 
